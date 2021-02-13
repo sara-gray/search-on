549c4092422881910408555e7969c8db13bfea65
Sara Gray <saragrayen@gmail.com>
2021-02-13 23:02:06 +0000

    Set up Navigation
    Using Router

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+	const links = [
+		{ id: 1, url: '/info', text: 'info' },
+		{ id: 2, url: '/play', text: 'play' },
+		{ id: 3, url: '/login', text: 'login' },
+	]
+	const drops = [
+		{ id: 1, url: '/info', text: 'info' },
+		{ id: 2, url: '/play', text: 'play' },
+		{ id: 3, url: '/edit', text: 'edit' },
+		{ id: 4, url: '/login', text: 'login' },
+	]
+	const [showLinks, setShowLinks] = useState(false)
+	const dropMenuRef = useRef(null)
+
+	const toggleMenu = () => {
+		setShowLinks(!showLinks)
+	}
+
+	useEffect(() => {
+		if (showLinks) {
+			dropMenuRef.current.style.display = 'inline'
+		} else {
+			dropMenuRef.current.style.display = 'none'
+		}
+	}, [showLinks])
+
+	return (
+		<nav>
+			<div className='nav-container section'>
+				<div className='logo'>
+					<h2>
+						<Link to='/'>SearchOn</Link>
+					</h2>
+				</div>
+				<div className='nav-toggle' onClick={toggleMenu}>
+					<FaBars />
+				</div>
+				<ul className='nav-links'>
+					{links.map((link) => {
+						const { id, url, text } = link
+						return (
+							<li key={id}>
+								<Link to={url}>{text}</Link>
+							</li>
+						)
+					})}
+				</ul>
+
+				<ul className='drop-links' ref={dropMenuRef}>
+					{drops.map((drop) => {
+						const { id, url, text } = drop
+						return (
+							<li key={id}>
+								<Link to={url}>{text}</Link>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
+		</nav>
+	)
+}
+
+export default Navigation
