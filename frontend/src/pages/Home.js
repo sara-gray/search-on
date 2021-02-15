9f75a3a58ed91c5be3094d2bda5942a9dbd188e9
Sara Gray <saragrayen@gmail.com>
2021-02-15 07:29:36 +0000

    working on hero

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
+	const MAX_HERO_IMAGES = 6
+	const randomImage =
+		Math.floor(Math.random() * Math.floor(MAX_HERO_IMAGES)) + 1
+
 	return (
-		<div>
-			<h1 style={{ textAlign: 'center' }}>Home</h1>
-		</div>
+		<section className={`hero hero-images${randomImage}`}>
+			<div className='hero-center'>
+				<article className='hero-info'>
+					<h2>Play and create Wordsearches online</h2>
+					<p>
+						<strong>Education</strong>: Create and distribute wordsearches to
+						you students
+					</p>
+					<button className='btn primary'>Login</button>
+				</article>
+				<article>2</article>
+			</div>
+			{/* <div className='hero-center'>
+				<article className='hero-info'>
+					<h2>Play and create Wordsearches online</h2>
+				
+					
+					<button className='btn'>Register</button>
+				</article>
+			</div> */}
+		</section>
 	)
 }
 
