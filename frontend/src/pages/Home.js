6e0e497e0ccf427ca2d509c04a0fe2d40fe56744
sara-gray <saragrayen@gmail.com>
2021-02-15 09:04:12 +0000

    Home phase 1 finished

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 	const MAX_HERO_IMAGES = 6
@@ -6,26 +7,29 @@ const Home = () => {
 		Math.floor(Math.random() * Math.floor(MAX_HERO_IMAGES)) + 1
 
 	return (
-		<section className={`hero hero-images${randomImage}`}>
-			<div className='hero-center'>
-				<article className='hero-info'>
+		<section className={`hero hero-image${randomImage}`}>
+			<div className='hero-center section'>
+				<article className='hero-info '>
 					<h2>Play and create Wordsearches online</h2>
 					<p>
 						<strong>Education</strong>: Create and distribute wordsearches to
 						you students
 					</p>
-					<button className='btn primary'>Login</button>
+					<button className='btn primary'>
+						<Link to='/login'>Login</Link>
+					</button>
+					<button className='btn'>
+						<Link to='/login'>Register</Link>
+					</button>
 				</article>
-				<article>2</article>
+				<article className='hero-cards'>
+					<div className='card '>1</div>
+					<div className='card '>2</div>
+					<div className='card '>3</div>
+					<div className='card'>4</div>
+					<div className='card'>5</div>
+				</article>
 			</div>
-			{/* <div className='hero-center'>
-				<article className='hero-info'>
-					<h2>Play and create Wordsearches online</h2>
-				
-					
-					<button className='btn'>Register</button>
-				</article>
-			</div> */}
 		</section>
 	)
 }
