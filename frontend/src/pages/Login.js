c4c1902f815d8f95732ab0c3b51d6984454a643f
Sara Gray <saragrayen@gmail.com>
2021-02-28 05:52:30 +0000

    login pages

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { notify } from '../components/notify'
 
 const Login = () => {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleClick = (e) => {
+		e.preventDefault()
+		alert('time to login')
+	}
+
 	return (
-		<div className='section' style={{ marginTop: '2rem' }}>
-			<div className=' card large-card'>
-				<h2>Coming soon...</h2>
-				<br />
-				<p>You will soon be able to login and build your own wordsearches.</p>
-				<p>Each grid is fully customisable: </p>
-				<ul style={{ listStyleType: 'circle' }}>
-					<li>Title</li>
-					<li>Description</li>
-					<li>
-						Words - you can include more than one word e.g. 'my name' will by
-						'myname' in the grid
-					</li>
-					<li>Grid size - horizontal by vertical</li>
-				</ul>
-			</div>
+		<div className='section'>
+			<form className='login-form'>
+				<h3>Login</h3>
+				<div className='form-control'>
+					<input
+						type='email'
+						className='login'
+						placeholder='Email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<input
+						type='password'
+						className='login'
+						placeholder='Password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<p style={{ fontSize: '0.8rem' }}>
+						Not registered? <Link to='/register'>Register</Link>
+					</p>
+					<button type='button' onClick={handleClick} className='btn primary'>
+						Login
+					</button>
+				</div>
+			</form>
 		</div>
 	)
 }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { notify } from '../components/notify'
 
 const Register = () => {
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [confirmPassword, setConfirmPassword] = useState('')
+
+	const handleClick = (e) => {
+		e.preventDefault()
+		alert('time to register')
+	}
+
 	return (
-		<div>
-			<h1 style={{ textAlign: 'center' }}>Register</h1>
+		<div className='section'>
+			<form className='login-form'>
+				<h3>Register</h3>
+				<div className='form-control'>
+					<input
+						type='text'
+						className='login'
+						placeholder='Name'
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+					<input
+						type='email'
+						className='login'
+						placeholder='Email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<input
+						type='password'
+						className='login'
+						placeholder='Password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+
+					<input
+						type='password'
+						className='login'
+						placeholder='Confirm password'
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					/>
+					<p style={{ fontSize: '0.8rem' }}>
+						Already registered? <Link to='/login'>Login</Link>
+					</p>
+					<button type='button' onClick={handleClick} className='btn primary'>
+						Register
+					</button>
+				</div>
+			</form>
 		</div>
 	)
 }
